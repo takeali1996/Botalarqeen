064c5d3c707fdfa4c9126d1475138221b2a67a6b
takeali1996 <krareuwsf1331@gmail.com>
2017-03-12 21:17:25 +0300

    Update chaneel.lua

--- plugins/chaneel.lua
+++ plugins/chaneel.lua
@@ -6,7 +6,7 @@ local receiver = get_receiver(msg)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
      leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
-       send_large_msg(receiver, 'ياعزيزي يافيطي لايمكنك انا تدخني هنا الى شخص واحد فقط هو @help_telp \n @smart_team : @devss_bot', ok_cb, false)
+       send_large_msg(receiver, 'سيتم طرد البوتات @TQ_QT \n  : @@Xotaqi_bot', ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
      leave_channel(receiver, ok_cb, false)
     end
@@ -14,8 +14,8 @@ end
  
 return {
   patterns = {
-    "^/(kickbot)$",
-    "^(kickbot)$",
+    "^/(طرد البوتات)$",
+    "^(طرد البوتات)$",
     "^!!tgservice (.+)$",
   },
   run = run
