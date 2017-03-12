94843d08e71c131eb6fccf4e610de40bb6127de3
takeali1996 <krareuwsf1331@gmail.com>
2017-03-12 21:24:26 +0300

    Update date.lua

--- plugins/date.lua
+++ plugins/date.lua
@@ -1,23 +1,23 @@
 do 
 local function run(msg,matches)
-return "😃 Hi "..msg.from.first_name.."\n"
-.."🌇Day "..os.date(' %a', os.time()).."\n"
-.."🏝Month "..os.date(' %b', os.time()).."\n"
-.."🎆Year "..os.date(' %Y', os.time()).."\n"
+return "🛐 هلو.اول  "..msg.from.first_name.."\n"
+.."🌇اليوم "..os.date(' %a', os.time()).."\n"
+.."🏝الشهر "..os.date(' %b', os.time()).."\n"
+.."🎆السنه "..os.date(' %Y', os.time()).."\n"
 .."🌄Num day "..os.date(' %d', os.time()).."\n"
-.."⏰Time "..os.date(' %p', os.time()).."\n"
-.."📅Hour "..os.date(' %I', os.time()).."\n"
-.."⏱Minute "..os.date(' %M', os.time()).."\n"
-.."🕯Sec "..os.date(' %S', os.time()).."\n"
-.."📿date "..os.date(' %x', os.time()).."\n"
-.."🛰Time "..os.date(' %X', os.time()).."\n"
-.."❤️ Dev By @smart_team & @Help_telp"
+.."⏰الوقت "..os.date(' %p', os.time()).."\n"
+.."📅الساعه "..os.date(' %I', os.time()).."\n"
+.."⏱الدقيقه "..os.date(' %M', os.time()).."\n"
+.."🕯الثانيه "..os.date(' %S', os.time()).."\n"
+.."📿التاريخ "..os.date(' %x', os.time()).."\n"
+.."🛰الوقت "..os.date(' %X', os.time()).."\n"
+.."❤️ Dev By @TQ_QT :™ قناه البوت :@TO_AT"
 end
 
 return {
 patterns = {
-"^[!/#](date)",
-"^(date)"
+"^[!/#](حول)",
+"^(الاوقات)"
 },
 run = run,
 }
